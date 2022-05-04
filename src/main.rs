9af8eb1721bbcfa5734498f0198c6cee10ad762c
Benjamin Guan <benjaminguan001@gmail.com>
2022-05-03 22:26:51 -0500

    added text input and styling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use yew::prelude::*;
+use gloo::timers::callback::{Interval};
 
 enum Msg {
-    AddOne,
+    UpdateTime,
+    InputValue(String),
+    Delete,
+    CheckTime,
+    PlaySound,
 }
 
 struct CounterComponent {
-    count: i64,
+    sleep_label: String,
+    sleep_time: String,
+    interval: Interval,
+    check_interval: Interval,
+    current_time: String,
+}
+
+fn get_current_time() -> String {
+    let date = js_sys::Date::new_0();
+    String::from(date.to_locale_time_string("en-US"))
 }
 
 impl Component for CounterComponent {
@@ -13,26 +27,60 @@ impl Component for CounterComponent {
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
+        let clock_handle = {
+            let link = _ctx.link().clone();
+            Interval::new(1, move || link.send_message(Msg::UpdateTime))
+        };
+        let check_handle = {
+            let link = _ctx.link().clone();
+            Interval::new(1, move || link.send_message(Msg::CheckTime))
+        };
+        let sl = "I want to sleep at... ";
+        let st = "hh:mm:ss AM/PM";
         Self {
-            count: 0,
+            sleep_time: st.to_string(),
+            sleep_label: sl.to_string(),
+            interval: clock_handle,
+            check_interval: check_handle,
+            current_time: get_current_time(),
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.count += 1;
-                true // re-render component
+            Msg::UpdateTime => {
+                self.current_time = get_current_time();
+                true
+            }
+            Msg::InputValue(value) => {
+                self.sleep_time += &value;
+                true
+            }
+            Msg::Delete => {
+                self.sleep_time = "".to_string();
+                true
+            }   
+            Msg::CheckTime => {
+                if self.sleep_time == self.current_time {
+                    _ctx.link().clone().send_message(Msg::PlaySound);
+                }
+                true
+            }
+            Msg::PlaySound => {
+                true
             }
         }
+
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
         html! {
             <div class="container">
-                <p>{ self.count }</p>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p> { self.current_time.clone() }</p>
+                <a> { self.sleep_label.clone() } </a>
+                <input type="text" value={self.sleep_time.clone()} oninput={link.callback(|e: InputEvent| Msg::InputValue(e.data().unwrap()))} />
+                <button onclick={link.callback(|_| Msg::Delete)}>{ '\u{232B}' }</button>
             </div>
         }
     }
